0bb76228a678d5296f975810e2e02398f04f87c0
minhoo03 <minhoo0333@gmail.com>
2021-07-24 07:07:50 +0900

    basic type

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,24 @@
+// JS 문자열 선언
+// let str = 'hello'
+
+// TS 문자열 선언
+let str: string = 'hello'
+
+let num: number = 10
+let arr: Array<number> = [1, 2, 3]
+// error
+// let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]
+let items: number[] = [1, 2, 3]
+
+// TS 튜플
+let address: [string, number] = ['gangnam', 100]
+
+// TS 객체
+let obj: object = {}
+let person: {name: string, age: number} = {
+    name: 'minhoo',
+    age: 19
+}
+
+// TS 진위값
+let show: boolean = true
