0e2110cdb9a6c6c1874d0dff164f0de5cb23ba0b
minhoo03 <minhoo0333@gmail.com>
2021-07-26 23:53:38 +0900

    TodoList-TS: >> index.ts << Set Type

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,41 +1,41 @@
-let todoItems;
+let todoItems: object[]
 
 // api
-function fetchTodoItems() {
+function fetchTodoItems(): object[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
     { id: 3, title: '스크립트', done: false },
-  ];
-  return todos;
+  ]
+  return todos
 }
 
 // crud methods
-function fetchTodos() {
-  const todos = fetchTodoItems();
-  return todos;
+function fetchTodos(): object[] {
+  const todos = fetchTodoItems()
+  return todos
 }
 
-function addTodo(todo) {
-  todoItems.push(todo);
+function addTodo(todo: object): void {
+  todoItems.push(todo)
 }
 
-function deleteTodo(index) {
-  todoItems.splice(index, 1);
+function deleteTodo(index: number): void {
+  todoItems.splice(index, 1)
 }
 
-function completeTodo(index, todo) {
-  todo.done = true;
-  todoItems.splice(index, 1, todo);
+function completeTodo(index: number, todo: object): void {
+  todo.done = true
+  todoItems.splice(index, 1, todo)
 }
 
 // business logic
-function logFirstTodo() {
-  return todoItems[0];
+function logFirstTodo(): object {
+  return todoItems[0]
 }
 
-function showCompleted() {
-  return todoItems.filter(item => item.done);
+function showCompleted(): object[] {
+  return todoItems.filter(item => item.done)
 }
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
@@ -44,10 +44,10 @@ function addTwoTodoItems() {
 }
 
 // NOTE: 유틸 함수
-function log() {
-  console.log(todoItems);
+function log(): void {
+  console.log(todoItems)
 }
 
-todoItems = fetchTodoItems();
-addTwoTodoItems();
-log();
+todoItems = fetchTodoItems()
+addTwoTodoItems()
+log()
