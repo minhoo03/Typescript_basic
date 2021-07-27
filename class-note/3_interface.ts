79cf2bfd2c77e7c78f25dc861f8353e882625d0d
minhoo03 <minhoo0333@gmail.com>
2021-07-28 03:25:16 +0900

    인덱싱

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,13 +3,13 @@ interface User {
     name: string;
 }
 
-// 변수에 인터페이스 활용
+// 0. 변수에 인터페이스 활용
 var minhoo: User = {
     age: 33,
     name: '민후'
 }
 
-// 함수에 인터페이스 활용
+// 1. 함수에 인터페이스 활용
 function getUser(user: User) {
     console.log(user)
 }
@@ -20,7 +20,7 @@ const sm = {
 }
 getUser(sm)
 
-// 함수의 스펙(구조)에 인터페이스를 활용
+// 2. 함수의 스펙(구조)에 인터페이스를 활용
 interface SumFunction {
     // 이 인터페이스는 인자를 아래와 같이 받게하는 인터페이스이다.
     (a: number, b: number): number
@@ -30,3 +30,14 @@ var sum: SumFunction
 sum = function(a, b) {
     return a + b
 }
+
+
+// 3. 인덱싱
+interface StringArray {
+    [index: number]: string
+}
+
+var arr: StringArray = ['a', 'b', 'c']
+arr[0] = 'str'
+// var aa: StringArray = {name1: 'string'} <- index가 string이기에 false
+// var bb: StringArray = {1: 'string'} <- index가 number이기에 true
