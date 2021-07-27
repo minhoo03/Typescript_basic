036ab43ddeda3c7834536fad6acdf3100df347db
minhoo03 <minhoo0333@gmail.com>
2021-07-27 22:56:04 +0900

    Use interface for function

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,19 @@ interface User {
     name: string;
 }
 
+// 변수에 인터페이스 활용
 var minhoo: User = {
     age: 33,
     name: '민후'
 }
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user)
+}
+
+const sm = {
+    age: 19,
+    name: 'ㅅㅁ'
+}
+getUser(sm)
