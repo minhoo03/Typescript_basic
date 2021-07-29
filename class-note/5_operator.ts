8931c97fe808879bea740e6f1422c982b1079e20
minhoo03 <minhoo0333@gmail.com>
2021-07-30 01:32:38 +0900

    유니온 타입 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,6 +1,15 @@
 // 유니온 타입 (하나 이상의 타입을 정할 수 있다.)
 function logMessage(value: string | number) {
-    console.log(value)
+    // 타입 가드: 아래처럼 특정 타입으로 범위를 좁혀나가는 과정
+    if (typeof value === 'number') {
+        value.toLocaleString()
+    }
+    // any와 다르게 각 타입에 맞는 메소드 추천
+    if (typeof value === 'string') {
+        value.toString()
+    }
+
+    throw new TypeError('value must be string or number')
 }
 
 logMessage('hello')
