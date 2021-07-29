1b901ca6e63fb53cd9dc407a4bd1550e63ecf63c
minhoo03 <minhoo0333@gmail.com>
2021-07-30 01:11:05 +0900

    타입 별칭 예제

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -11,7 +11,6 @@ var minhoo: User = {
 
 
 
-
 // 1. 함수에 인터페이스 활용
 function getUser(user: User) {
     console.log(user)
---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,22 @@
+// interface Human {
+//     name: string,
+//     age: number,
+// }
+
+type Human = {
+    name: string,
+    age: number,
+}
+
+var minhoo: Human = {
+    name: "민후",
+    age: 19
+}
+
+type MyString = string
+var str: MyString = 'hello'
+
+type Todo = { id: string, title: string, done: boolean }
+function getTodo(todo: Todo) {
+    
+}
