fdbe4b261180be673c49780d7a81f3074d2ca39a
minhoo03 <minhoo0333@gmail.com>
2021-07-31 00:24:23 +0900

    두 타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -29,7 +29,7 @@ interface Person {
 }
 
 function askSomeone1(someone: Developer | Person) {
-    someone.name
+    // someone.name
 }
 
 
@@ -37,7 +37,16 @@ function askSomeone1(someone: Developer | Person) {
 // 1. 인터섹션 타입
 // 유니온과 다르게 두 타입을 모두 적용해야 하기에 모든 멤버 변수 사용 가능
 function askSomeone2(someone: Developer & Person) {
-    someone.name
-    someone.age
-    someone.skill
+    // someone.name
+    // someone.age
+    // someone.skill
 }
+
+
+
+// 2. 두 타입의 차이점
+    // 둘 중 한 타입의 모습만 갖추면 된다.
+askSomeone1({ name: '디벨로퍼', skill: '웹 개발'})
+askSomeone1({ name: '미누', age: 19})
+    // 모든 타입이 적용되어야 함
+askSomeone2({ name: '민후', skill: '웹 개발', age: 19})
