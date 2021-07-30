4289b5a1019c69b7911c1a51791b298877c1692c
minhoo03 <minhoo0333@gmail.com>
2021-07-31 00:20:36 +0900

    인터섹션 타입

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,4 +1,4 @@
-// 유니온 타입 (하나 이상의 타입을 정할 수 있다.)
+// 0. 유니온 타입 (하나 이상의 타입을 정할 수 있다.)
 function logMessage(value: string | number) {
     // 타입 가드: 아래처럼 특정 타입으로 범위를 좁혀나가는 과정
     if (typeof value === 'number') {
@@ -28,6 +28,16 @@ interface Person {
     age: number
 }
 
-function askSomeone(someone: Developer | Person) {
+function askSomeone1(someone: Developer | Person) {
     someone.name
+}
+
+
+
+// 1. 인터섹션 타입
+// 유니온과 다르게 두 타입을 모두 적용해야 하기에 모든 멤버 변수 사용 가능
+function askSomeone2(someone: Developer & Person) {
+    someone.name
+    someone.age
+    someone.skill
 }
