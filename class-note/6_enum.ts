0a6b24acb74df4813cc1836da39ce1ebedfb5d60
minhoo03 <minhoo0333@gmail.com>
2021-07-31 01:17:14 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -3,9 +3,23 @@ enum Shoes {
     // 별도의 값을 정하지 않으면 숫자형으로 취급한다
     Nike, // 0
     Adidas, // 1
-    Fuma // 2
+    Fuma, // 2
+}
+
+enum Test {
+    T1 = 10,
+    T2, // 11
+    T3 // 12
 }
 
 let myShoes = Shoes.Nike
-
 console.log(myShoes) // 0
+
+
+// 문자형
+enum Brand {
+    FCMM = 'FCMM',
+    FLION = 'FLION'
+}
+
+console.log(Brand.FCMM)
