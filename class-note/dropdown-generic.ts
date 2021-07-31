12262bac3309d61ff26a6b0c73e12041fce5667a
minhoo03 <minhoo0333@gmail.com>
2021-07-31 23:25:41 +0900

    제네릭 실전 예제

---
+++ class-note/dropdown-generic.ts
@@ -0,0 +1,45 @@
+interface Email {
+  value: string,
+  selected: boolean
+}
+
+const emails: Email[] = [
+  { value: 'naver.com', selected: true },
+  { value: 'gmail.com', selected: false },
+  { value: 'hanmail.net', selected: false },
+];
+
+interface ProductNumber {
+  value: number,
+  selected: boolean
+}
+
+const numberOfProducts: ProductNumber[] = [
+  { value: 1, selected: true },
+  { value: 2, selected: false },
+  { value: 3, selected: false },
+];
+
+// ===========================================
+
+function createDropdownItem(item: Email | ProductNumber) {
+  const option = document.createElement('option');
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+
+// NOTE: 이메일 드롭 다운 아이템 추가
+emails.forEach(function (email) {
+  const item = createDropdownItem(email);
+  const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+// NOTE: 넘버 드롭 다운 아이템 추가
+numberOfProducts.forEach(product => {
+  const item = createDropdownItem(product)
+  const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+})
