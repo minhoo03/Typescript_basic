8b790394e08140d41c82944291e6e6de41eb9ae8
minhoo03 <minhoo0333@gmail.com>
2021-08-03 00:47:02 +0900

    복잡한 구조의 타입 추론

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -7,3 +7,36 @@ function getB(b = 10) { // b는 기본적으로 10
 }
 
 10 + '10' // '1010'
+
+
+
+// 인터페이스와 제네릭을 이용한 타입 추론
+interface Dropdown<T> {
+    value: T,
+    title: string
+}
+  // <string>을 통해 value가 string일 것을 추론
+let shoppingItem: Dropdown<string> = {
+    value: 'abc',
+    title: 'hello'
+}
+
+let shoppingPrice: Dropdown<number> = {
+    value: 1000,
+    title: 'hi'
+}
+
+
+
+// 복잡한 구조에서 타입 추론
+interface DetailedDropdown<K> extends Dropdown<K> {
+    description: string,
+    tag: K
+}
+  // Dropdown의 제네릭도 string임을 추론
+let detatiledItem: DetailedDropdown<string> = {
+    value: 'test',
+    title: 'Nice',
+    description: 'desc',
+    tag: 'tag123'
+}
